1fbf5c364f2df895d3215828a7c1abadf06d5260
Strivin0311 <hyp@smail.nju.edu.cn>
2025-06-10 07:57:07 +0000

    updated script in intro0 asyncAPI

--- Samples/0_Introduction/asyncAPI/build_and_run.sh
+++ Samples/0_Introduction/asyncAPI/build_and_run.sh
@@ -1,12 +1,15 @@
 
 SEP="--------------------------------------------------------------------"
+
+BUILD_ROOT=build
 TARGET=asyncAPI
+
 
 echo "$SEP"
 echo "Building ${TARGET}"
 echo "$SEP"
 
-mkdir -p build && cd build || exit
+mkdir -p $BUILD_ROOT && cd $BUILD_ROOT || exit
 
 cmake ..
 
@@ -14,13 +17,15 @@ make -j8 || exit
 
 cd ..
 
+
 echo "$SEP"
 echo "Running ${TARGET}"
 echo "$SEP"
 
-CMD=./build/${TARGET}
+CMD=./$BUILD_ROOT/$TARGET
 
 $CMD
+
 
 echo "$SEP"
 echo "Profiling ${TARGET}"
@@ -31,6 +36,7 @@ nsys profile \
     -o ${TARGET}.nsys-rep \
     --capture-range=cudaProfilerApi \
     $CMD
+
 
 echo "$SEP"
 echo "Done"
