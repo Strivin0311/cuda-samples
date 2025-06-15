65a75aba84da8714427da01f29edf4a037a94ac6
Strivin0311 <hyp@smail.nju.edu.cn>
2025-06-15 08:54:44 +0000

    run through matmul with dynamic link and jit dump

--- Samples/0_Introduction/matrixMul_nvrtc/matrixMul_kernel.cu
+++ Samples/0_Introduction/matrixMul_nvrtc/matrixMul_kernel.cu
@@ -47,85 +47,104 @@
 * NOTE: A and B matrices are all stored in row-major order
  */
 
-#include <cooperative_groups.h>
+ #include <cooperative_groups.h>
 
-template <int BLOCK_SIZE> __device__ void matrixMulCUDA(float *C, float *A, float *B, int wA, int wB)
-{
-    // Handle to thread block group, i.e. cooperative thread array (cta)
-    cooperative_groups::thread_block cta = cooperative_groups::this_thread_block();
-    // Block index
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-
-    // Thread index
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-
-    // Index of the first sub-matrix of A processed by the block
-    int aBegin = wA * BLOCK_SIZE * by;
-
-    // Index of the last sub-matrix of A processed by the block
-    int aEnd = aBegin + wA - 1;
-
-    // Step size used to iterate through the sub-matrices of A
-    int aStep = BLOCK_SIZE;
-
-    // Index of the first sub-matrix of B processed by the block
-    int bBegin = BLOCK_SIZE * bx;
-
-    // Step size used to iterate through the sub-matrices of B
-    int bStep = BLOCK_SIZE * wB;
-
-    // Csub is used to store the element of the block sub-matrix
-    // that is computed by the thread
-    float Csub = 0;
-
-    // Loop over all the sub-matrices of A and B
-    // required to compute the block sub-matrix
-    for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
-        // Declaration of the shared memory array As used to
-        // store the sub-matrix of A
-        __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
-
-        // Declaration of the shared memory array Bs used to
-        // store the sub-matrix of B
-        __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
-
-        // Load the matrices from device memory
-        // to shared memory; each thread loads
-        // one element of each matrix
-        As[ty][tx] = A[a + wA * ty + tx];
-        Bs[ty][tx] = B[b + wB * ty + tx];
-
-        // Synchronize this block to make sure the matrices are loaded
-        cooperative_groups::sync(cta);
-
-        // Multiply the two matrices together;
-        // each thread computes one element
-        // of the block sub-matrix
-        #pragma unroll
-        for (int k = 0; k < BLOCK_SIZE; ++k) {
-            Csub += As[ty][k] * Bs[k][tx];
-        }
-
-        // Synchronize this block to make sure that the preceding
-        // computation is done before loading two new
-        // sub-matrices of A and B in the next iteration
-        cooperative_groups::sync(cta);
-    }
-
-    // Write the block sub-matrix to device memory;
-    // each thread writes one element
-    int c               = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-    C[c + wB * ty + tx] = Csub;
-}
-
-extern "C" __global__ void matrixMulCUDA_block16(float *C, float *A, float *B, int wA, int wB)
-{
-    matrixMulCUDA<16>(C, A, B, wA, wB);
-}
-
-extern "C" __global__ void matrixMulCUDA_block32(float *C, float *A, float *B, int wA, int wB)
-{
-    matrixMulCUDA<32>(C, A, B, wA, wB);
-}
+ template <int BLOCK_SIZE> __device__ void matrixMulCUDA(float *C, float *A, float *B, int wA, int wB)
+ {
+     // Handle to thread block group, i.e. cooperative thread array (cta)
+     cooperative_groups::thread_block cta = cooperative_groups::this_thread_block();
+     // Block index
+     int bx = blockIdx.x;
+     int by = blockIdx.y;
+ 
+     // Thread index
+     int tx = threadIdx.x;
+     int ty = threadIdx.y;
+ 
+     /** For 2D grid and 2D thread block, the global thread id is calculated as:
+      * step1. bid = blockIdx.y * gridDim.x + blockIdx.x // flatten block id
+      * step2. bsz = blockDim.y * blockDim.x // flatten block size
+      * step3. stid = bid * bsz // starting thread id of this block
+      * step4. tid_offset = threadIdx.y * blockDim.x + threadIdx.x // flatten thread id offset in this block
+      * step5. tid = stid + tid_offset
+      */
+     int local_tid = threadIdx.y * blockDim.x + threadIdx.x;
+     int global_tid = (blockIdx.y * gridDim.x + blockIdx.x) \
+         * blockDim.x * blockDim.y \
+         + local_tid;
+     if (global_tid == 0) {
+         printf("Running grid with gridDim.x=%d, gridDim.y=%d, blockDim.x=%d, blockDim.y=%d\n", gridDim.x, gridDim.y, blockDim.x, blockDim.y);
+     }
+     if (local_tid == 0) {
+         printf("Running block with block id (%d, %d)\n", blockIdx.x, blockIdx.y);
+     }
+ 
+     // Index of the first sub-matrix of A processed by the block
+     int aBegin = wA * BLOCK_SIZE * by;
+ 
+     // Index of the last sub-matrix of A processed by the block
+     int aEnd = aBegin + wA - 1;
+ 
+     // Step size used to iterate through the sub-matrices of A
+     int aStep = BLOCK_SIZE;
+ 
+     // Index of the first sub-matrix of B processed by the block
+     int bBegin = BLOCK_SIZE * bx;
+ 
+     // Step size used to iterate through the sub-matrices of B
+     int bStep = BLOCK_SIZE * wB;
+ 
+     // Csub is used to store the element of the block sub-matrix
+     // that is computed by the thread
+     float Csub = 0;
+ 
+     // Loop over all the sub-matrices of A and B
+     // required to compute the block sub-matrix
+     for (int a = aBegin, b = bBegin; a <= aEnd; a += aStep, b += bStep) {
+         // Declaration of the shared memory array As used to
+         // store the sub-matrix of A
+         __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+ 
+         // Declaration of the shared memory array Bs used to
+         // store the sub-matrix of B
+         __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+ 
+         // Load the matrices from device memory
+         // to shared memory; each thread loads
+         // one element of each matrix
+         As[ty][tx] = A[a + wA * ty + tx];
+         Bs[ty][tx] = B[b + wB * ty + tx];
+ 
+         // Synchronize this block to make sure the matrices are loaded
+         cooperative_groups::sync(cta);
+ 
+         // Multiply the two matrices together;
+         // each thread computes one element
+         // of the block sub-matrix
+         #pragma unroll
+         for (int k = 0; k < BLOCK_SIZE; ++k) {
+             Csub += As[ty][k] * Bs[k][tx];
+         }
+ 
+         // Synchronize this block to make sure that the preceding
+         // computation is done before loading two new
+         // sub-matrices of A and B in the next iteration
+         cooperative_groups::sync(cta);
+     }
+ 
+     // Write the block sub-matrix to device memory;
+     // each thread writes one element
+     int c               = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
+     C[c + wB * ty + tx] = Csub;
+ }
+ 
+ extern "C" __global__ void matrixMulCUDA_block16(float *C, float *A, float *B, int wA, int wB)
+ {
+     matrixMulCUDA<16>(C, A, B, wA, wB);
+ }
+ 
+ extern "C" __global__ void matrixMulCUDA_block32(float *C, float *A, float *B, int wA, int wB)
+ {
+     matrixMulCUDA<32>(C, A, B, wA, wB);
+ }
+ 
