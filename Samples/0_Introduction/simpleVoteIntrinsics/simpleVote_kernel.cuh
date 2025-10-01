64e6b0547d37f7b0adbdaa9bae6c6ea2c9d6ccda
Strivin0311 <hyp@smail.nju.edu.cn>
2025-10-01 10:14:24 +0000

    tried simpleVoteIntrinsics

--- Samples/0_Introduction/simpleVoteIntrinsics/simpleVote_kernel.cuh
+++ Samples/0_Introduction/simpleVoteIntrinsics/simpleVote_kernel.cuh
@@ -36,8 +36,7 @@
 
 // Kernel #1 tests the across-the-warp vote(any) intrinsic.
 // If ANY one of the threads (within the warp) of the predicated condition
-// returns a non-zero value, then all threads within this warp will return a
-// non-zero value
+// returns a non-zero value, then all threads within this warp will return a non-zero value
 __global__ void VoteAnyKernel1(unsigned int *input, unsigned int *result, int size)
 {
     int tx = threadIdx.x;
@@ -48,8 +47,7 @@ __global__ void VoteAnyKernel1(unsigned int *input, unsigned int *result, int si
 
 // Kernel #2 tests the across-the-warp vote(all) intrinsic.
 // If ALL of the threads (within the warp) of the predicated condition returns
-// a non-zero value, then all threads within this warp will return a non-zero
-// value
+// a non-zero value, then all threads within this warp will return a non-zero value
 __global__ void VoteAllKernel2(unsigned int *input, unsigned int *result, int size)
 {
     int tx = threadIdx.x;
