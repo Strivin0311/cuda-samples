7ed2448fb6622237eb4470dbae663b362c838393
Strivin0311 <hyp@smail.nju.edu.cn>
2025-10-07 11:41:32 +0000

    tried simpleCudaGraphs

--- Samples/3_CUDA_Features/simpleCudaGraphs/simpleCudaGraphs.cu
+++ Samples/3_CUDA_Features/simpleCudaGraphs/simpleCudaGraphs.cu
@@ -167,15 +167,19 @@ void cudaGraphsManual(float  *inputVec_h,
     cudaMemcpy3DParms    memcpyParams     = {0};
     cudaMemsetParams     memsetParams     = {0};
 
+    // init memcpy params (inputVec_h -> inputVec_d)
     memcpyParams.srcArray = NULL;
     memcpyParams.srcPos   = make_cudaPos(0, 0, 0);
     memcpyParams.srcPtr   = make_cudaPitchedPtr(inputVec_h, sizeof(float) * inputSize, inputSize, 1);
+    
     memcpyParams.dstArray = NULL;
     memcpyParams.dstPos   = make_cudaPos(0, 0, 0);
     memcpyParams.dstPtr   = make_cudaPitchedPtr(inputVec_d, sizeof(float) * inputSize, inputSize, 1);
+    
     memcpyParams.extent   = make_cudaExtent(sizeof(float) * inputSize, 1, 1);
     memcpyParams.kind     = cudaMemcpyHostToDevice;
 
+    // init memset params (outputVec_d -> 0)
     memsetParams.dst         = (void *)outputVec_d;
     memsetParams.value       = 0;
     memsetParams.pitch       = 0;
@@ -183,6 +187,10 @@ void cudaGraphsManual(float  *inputVec_h,
     memsetParams.width       = numOfBlocks * 2;
     memsetParams.height      = 1;
 
+    // add memcpy and memset nodes (node0, node1), w/o dependencies
+    // i.e. there are two root nodes:
+    // node0 -> ...
+    // node1 -> ...
     checkCudaErrors(cudaGraphCreate(&graph, 0));
     checkCudaErrors(cudaGraphAddMemcpyNode(&memcpyNode, graph, NULL, 0, &memcpyParams));
     checkCudaErrors(cudaGraphAddMemsetNode(&memsetNode, graph, NULL, 0, &memsetParams));
@@ -190,6 +198,7 @@ void cudaGraphsManual(float  *inputVec_h,
     nodeDependencies.push_back(memsetNode);
     nodeDependencies.push_back(memcpyNode);
 
+    // init kernel params (reduce kernel)
     void *kernelArgs[4] = {(void *)&inputVec_d, (void *)&outputVec_d, &inputSize, &numOfBlocks};
 
     kernelNodeParams.func           = (void *)reduce;
@@ -199,22 +208,29 @@ void cudaGraphsManual(float  *inputVec_h,
     kernelNodeParams.kernelParams   = (void **)kernelArgs;
     kernelNodeParams.extra          = NULL;
 
+    // add kernel node (node2), with the parent memcpy and memset as dependencies
+    // node0 --  .
+    //           V
+    // node1 ->  node2
     checkCudaErrors(cudaGraphAddKernelNode(
         &kernelNode, graph, nodeDependencies.data(), nodeDependencies.size(), &kernelNodeParams));
 
     nodeDependencies.clear();
     nodeDependencies.push_back(kernelNode);
 
+    // init memset params (result_d -> 0)
     memset(&memsetParams, 0, sizeof(memsetParams));
     memsetParams.dst         = result_d;
     memsetParams.value       = 0;
     memsetParams.elementSize = sizeof(float);
     memsetParams.width       = 2;
     memsetParams.height      = 1;
-    checkCudaErrors(cudaGraphAddMemsetNode(&memsetNode, graph, NULL, 0, &memsetParams));
 
+    // add memset node (node3)
+    checkCudaErrors(cudaGraphAddMemsetNode(&memsetNode, graph, NULL, 0, &memsetParams));
     nodeDependencies.push_back(memsetNode);
 
+    // init kernel params (reduceFinal kernel)
     memset(&kernelNodeParams, 0, sizeof(kernelNodeParams));
     kernelNodeParams.func           = (void *)reduceFinal;
     kernelNodeParams.gridDim        = dim3(1, 1, 1);
@@ -224,13 +240,19 @@ void cudaGraphsManual(float  *inputVec_h,
     kernelNodeParams.kernelParams   = kernelArgs2;
     kernelNodeParams.extra          = NULL;
 
+    // add kernel node (node4), with the parent kernel node and memset node as dependency
+    // node0 --  .
+    //           V
+    // node1 ->  node2 -- .
+    //                    V
+    //           node3 -> node4
     checkCudaErrors(cudaGraphAddKernelNode(
         &kernelNode, graph, nodeDependencies.data(), nodeDependencies.size(), &kernelNodeParams));
     nodeDependencies.clear();
     nodeDependencies.push_back(kernelNode);
 
+    // init memcpy params (result_d -> result_h)
     memset(&memcpyParams, 0, sizeof(memcpyParams));
-
     memcpyParams.srcArray = NULL;
     memcpyParams.srcPos   = make_cudaPos(0, 0, 0);
     memcpyParams.srcPtr   = make_cudaPitchedPtr(result_d, sizeof(double), 1, 1);
@@ -239,11 +261,19 @@ void cudaGraphsManual(float  *inputVec_h,
     memcpyParams.dstPtr   = make_cudaPitchedPtr(&result_h, sizeof(double), 1, 1);
     memcpyParams.extent   = make_cudaExtent(sizeof(double), 1, 1);
     memcpyParams.kind     = cudaMemcpyDeviceToHost;
+
+    // add memcpy node (node5), with the parent kernel node as dependency
+    // node0 --  .
+    //           V
+    // node1 ->  node2 -- .
+    //                    V
+    //           node3 -> node4 -> node5
     checkCudaErrors(
         cudaGraphAddMemcpyNode(&memcpyNode, graph, nodeDependencies.data(), nodeDependencies.size(), &memcpyParams));
     nodeDependencies.clear();
     nodeDependencies.push_back(memcpyNode);
 
+    // init host params (myHostNodeCallback func)
     cudaGraphNode_t    hostNode;
     cudaHostNodeParams hostParams = {0};
     hostParams.fn                 = myHostNodeCallback;
@@ -252,34 +282,52 @@ void cudaGraphsManual(float  *inputVec_h,
     hostFnData.fn_name  = "cudaGraphsManual";
     hostParams.userData = &hostFnData;
 
+    // add host node (node6), with the parent memcpy node as dependency
+    // node0 --  .
+    //           V
+    // node1 ->  node2 -- .
+    //                    V
+    //           node3 -> node4 -> node5 -> node6
     checkCudaErrors(
         cudaGraphAddHostNode(&hostNode, graph, nodeDependencies.data(), nodeDependencies.size(), &hostParams));
 
+    // get all nodes from the graph (node0~node6)
     cudaGraphNode_t *nodes    = NULL;
     size_t           numNodes = 0;
     checkCudaErrors(cudaGraphGetNodes(graph, nodes, &numNodes));
     printf("\nNum of nodes in the graph created manually = %zu\n", numNodes);
 
+    // instantiate the graph as below:
+    // node0 --  .
+    //           V
+    // node1 ->  node2 -- .
+    //                    V
+    //           node3 -> node4 -> node5 -> node6
     cudaGraphExec_t graphExec;
     checkCudaErrors(cudaGraphInstantiate(&graphExec, graph, NULL, NULL, 0));
 
+    // clone the graph
     cudaGraph_t     clonedGraph;
     cudaGraphExec_t clonedGraphExec;
     checkCudaErrors(cudaGraphClone(&clonedGraph, graph));
+
+    // instantiate the cloned graph
     checkCudaErrors(cudaGraphInstantiate(&clonedGraphExec, clonedGraph, NULL, NULL, 0));
 
+    // launch the graph multiple times on the given stream
     for (int i = 0; i < GRAPH_LAUNCH_ITERATIONS; i++) {
         checkCudaErrors(cudaGraphLaunch(graphExec, streamForGraph));
     }
-
     checkCudaErrors(cudaStreamSynchronize(streamForGraph));
 
+    // launch the cloned graph multiple times on the given stream
     printf("Cloned Graph Output.. \n");
     for (int i = 0; i < GRAPH_LAUNCH_ITERATIONS; i++) {
         checkCudaErrors(cudaGraphLaunch(clonedGraphExec, streamForGraph));
     }
     checkCudaErrors(cudaStreamSynchronize(streamForGraph));
 
+    // destroy the graph and cloned graph, as well as the stream
     checkCudaErrors(cudaGraphExecDestroy(graphExec));
     checkCudaErrors(cudaGraphExecDestroy(clonedGraphExec));
     checkCudaErrors(cudaGraphDestroy(graph));
@@ -294,7 +342,13 @@ void cudaGraphsUsingStreamCapture(float  *inputVec_h,
                                   size_t  inputSize,
                                   size_t  numOfBlocks)
 {
+    // stream1 for input memcpy, kernel, host callback
+    // stream2 for partial output memset
+    // stream3 for final result memset
     cudaStream_t stream1, stream2, stream3, streamForGraph;
+    // forkStreamEvent for beginning of the graph capture on stream1, waited before memset
+    // memsetEvent1 for partial output memset on stream2, waited before launching reduce kernel
+    // memsetEvent2 for final result memset on stream3, waited before launching reduceFinal kernel
     cudaEvent_t  forkStreamEvent, memsetEvent1, memsetEvent2;
     cudaGraph_t  graph;
     double       result_h = 0.0;
@@ -308,69 +362,124 @@ void cudaGraphsUsingStreamCapture(float  *inputVec_h,
     checkCudaErrors(cudaEventCreate(&memsetEvent1));
     checkCudaErrors(cudaEventCreate(&memsetEvent2));
 
+    // begin to caputre the graph on stream1
     checkCudaErrors(cudaStreamBeginCapture(stream1, cudaStreamCaptureModeGlobal));
 
+    // stream2, stream3 wait for forkStreamEvent recorded on stream1
     checkCudaErrors(cudaEventRecord(forkStreamEvent, stream1));
     checkCudaErrors(cudaStreamWaitEvent(stream2, forkStreamEvent, 0));
     checkCudaErrors(cudaStreamWaitEvent(stream3, forkStreamEvent, 0));
 
+    // memcpy (inputVec_h -> inputVec_d) on stream1
+    // named node0 as one the root nodes
+    // node0 -> ...
     checkCudaErrors(cudaMemcpyAsync(inputVec_d, inputVec_h, sizeof(float) * inputSize, cudaMemcpyDefault, stream1));
 
+    // memset (outputVec_d -> 0) on stream2
+    // named node1 as one the root nodes
+    // node0 -> ...
+    // node1 -> ...
     checkCudaErrors(cudaMemsetAsync(outputVec_d, 0, sizeof(double) * numOfBlocks, stream2));
-
     checkCudaErrors(cudaEventRecord(memsetEvent1, stream2));
 
+    // memset (result_d -> 0) on stream3
+    // named node2 as one the root nodes
+    // node0 -> ...
+    // node1 -> ...
+    // node2 -> ...
     checkCudaErrors(cudaMemsetAsync(result_d, 0, sizeof(double), stream3));
     checkCudaErrors(cudaEventRecord(memsetEvent2, stream3));
 
+    // stream1 wait for memsetEvent1 recorded on stream2
     checkCudaErrors(cudaStreamWaitEvent(stream1, memsetEvent1, 0));
 
+    // reduce kernel on stream1
+    // named node3, and it is dependent on node0 (same stream) and node1 (event wait)
+    // node0 --  .
+    //           V
+    // node1 ->  node3
+    // node2 -> ...
     reduce<<<numOfBlocks, THREADS_PER_BLOCK, 0, stream1>>>(inputVec_d, outputVec_d, inputSize, numOfBlocks);
 
+    // stream1 wait for memsetEvent2 recorded on stream3
     checkCudaErrors(cudaStreamWaitEvent(stream1, memsetEvent2, 0));
 
+    // reduceFinal kernel on stream1
+    // named node4, and it is dependent on node3 (same stream) and node2 (event wait)
+    // node0 --  .
+    //           V
+    // node1 ->  node3 -- .
+    //                    V
+    //           node2 -> node4
     reduceFinal<<<1, THREADS_PER_BLOCK, 0, stream1>>>(outputVec_d, result_d, numOfBlocks);
+
+    // memcpy (result_d -> result_h) on stream1
+    // named node5, and it is dependent on node4 (same stream)
+    // node0 --  .
+    //           V
+    // node1 ->  node3 -- .
+    //                    V
+    //           node2 -> node4 -> node5
     checkCudaErrors(cudaMemcpyAsync(&result_h, result_d, sizeof(double), cudaMemcpyDefault, stream1));
 
+    // host callback on stream1
+    // named node6, and it is dependent on node5 (same stream)
+    // node0 --  .
+    //           V
+    // node1 ->  node3 -- .
+    //                    V
+    //           node2 -> node4 -> node5 -> node6
     callBackData_t hostFnData = {0};
     hostFnData.data           = &result_h;
     hostFnData.fn_name        = "cudaGraphsUsingStreamCapture";
     cudaHostFn_t fn           = myHostNodeCallback;
     checkCudaErrors(cudaLaunchHostFunc(stream1, fn, &hostFnData));
+
+    // end to caputre the graph on stream1
     checkCudaErrors(cudaStreamEndCapture(stream1, &graph));
 
+    // get all the nodes in the graph (node0~node6)
     cudaGraphNode_t *nodes    = NULL;
     size_t           numNodes = 0;
     checkCudaErrors(cudaGraphGetNodes(graph, nodes, &numNodes));
     printf("\nNum of nodes in the graph created using stream capture API = %zu\n", numNodes);
 
+    // instantiate the graph
+    // node0 --  .
+    //           V
+    // node1 ->  node3 -- .
+    //                    V
+    //           node2 -> node4 -> node5 -> node6
     cudaGraphExec_t graphExec;
     checkCudaErrors(cudaGraphInstantiate(&graphExec, graph, NULL, NULL, 0));
 
+    // clone the graph
     cudaGraph_t     clonedGraph;
     cudaGraphExec_t clonedGraphExec;
     checkCudaErrors(cudaGraphClone(&clonedGraph, graph));
     checkCudaErrors(cudaGraphInstantiate(&clonedGraphExec, clonedGraph, NULL, NULL, 0));
 
+    // launch the graph multiple times on another stream
     for (int i = 0; i < GRAPH_LAUNCH_ITERATIONS; i++) {
         checkCudaErrors(cudaGraphLaunch(graphExec, streamForGraph));
     }
-
     checkCudaErrors(cudaStreamSynchronize(streamForGraph));
 
+    // launch the cloned graph multiple times on another stream
     printf("Cloned Graph Output.. \n");
     for (int i = 0; i < GRAPH_LAUNCH_ITERATIONS; i++) {
         checkCudaErrors(cudaGraphLaunch(clonedGraphExec, streamForGraph));
     }
-
     checkCudaErrors(cudaStreamSynchronize(streamForGraph));
 
+    // destroy the graph and cloned graph, as well as the streams
     checkCudaErrors(cudaGraphExecDestroy(graphExec));
     checkCudaErrors(cudaGraphExecDestroy(clonedGraphExec));
     checkCudaErrors(cudaGraphDestroy(graph));
     checkCudaErrors(cudaGraphDestroy(clonedGraph));
     checkCudaErrors(cudaStreamDestroy(stream1));
     checkCudaErrors(cudaStreamDestroy(stream2));
+    checkCudaErrors(cudaStreamDestroy(stream3));
     checkCudaErrors(cudaStreamDestroy(streamForGraph));
 }
 
